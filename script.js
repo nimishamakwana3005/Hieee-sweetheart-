2f5e7b8b09c71d39e9e2d80472c0acaff14437e9
nimishamakwana3005 <nimishamakwana3005@gmail.com>
2026-02-13 16:05:50 +0530

    Add popup message functions and navigation

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ function startLove() {
     window.location.href = "month1.html";
   }, 1500);
 }
+function showMessage() {
+  document.getElementById("popup").style.display = "block";
+}
+
+function closeMessage() {
+  document.getElementById("popup").style.display = "none";
+}
+
+function goNext() {
+  window.location.href = "month2.html";
+}
