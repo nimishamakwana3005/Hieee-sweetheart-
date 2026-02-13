991e7ef36dca9ab1ea90cca366c53144dce137cb
nimishamakwana3005 <nimishamakwana3005@gmail.com>
2026-02-13 16:00:06 +0530

    Remove welcome alert and add page redirect
    Removed welcome alert and added redirect after music plays.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function startLove() {
   const music = document.getElementById("bgMusic");
   music.play();
-  alert("Welcome to our 7 Days of Us 💜✨");
+
+  setTimeout(() => {
+    window.location.href = "month1.html";
+  }, 1500);
 }
